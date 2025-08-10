a26c448e1b47df22ed08041622d65e54b7adbeae
Muhammad M. Elsherbini <61868581+Muhammad-Elsherbini@users.noreply.github.com>
2025-08-10 13:32:05 +0300

    Update auth.py

--- utils/auth.py
+++ utils/auth.py
@@ -1,1 +1,19 @@
+import streamlit as st
+import pandas as pd
+
+def load_users():
+    return pd.read_csv("data/users.csv")
+
+def login():
+    st.title("🔐 Login")
+    users = load_users()
+    username = st.text_input("Username")
+    password = st.text_input("Password", type="password")
+    
+    if st.button("Login"):
+        if ((users['username'] == username) & (users['password'] == password)).any():
+            st.session_state["logged_in"] = True
+            st.experimental_rerun()
+        else:
+            st.error("Invalid credentials")
 
