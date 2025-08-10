322062f65c05630572112c565dd65a4a92ddef8e
Muhammad M. Elsherbini <61868581+Muhammad-Elsherbini@users.noreply.github.com>
2025-08-10 14:34:08 +0300

    Update 1_Dashboard.py

--- pages/1_Dashboard.py
+++ pages/1_Dashboard.py
@@ -1,5 +1,30 @@
+
+
 import streamlit as st
+from utils.db import run_query
 
-st.title("📊 Dashboard")
-st.write("Company analysis will go here.")
+st.title("📊 Company Dashboard")
 
+company = st.text_input("Enter company name:")
+
+if st.button("Load Data"):
+    query = f"""
+            SELECT  
+    		fin.[PERIOD_ID],
+    		fin.[VIEW_ID],
+    		vw.VIEW_LABEL,
+    		MAX(fin.[INSERT_DATE]) AS INSERT_DATE,
+    		MAX(fin.[UPDATE_DATE]) As UPDATE_DATE
+        FROM [CDR_DEV_WAREHOUSE].[ONEHFM].[ONEHFM_MAFPTB_F_FINANCIAL_FINAL] as fin
+        LEFT JOIN [CDR_DEV_WAREHOUSE].[ONEHFM].[onehfm_mafptb_d_value] as val ON val.VALUE_ID = fin.VALUE_ID
+        LEFT JOIN [CDR_DEV_WAREHOUSE].[ONEHFM].[onehfm_mafptb_d_view] as vw ON vw.VIEW_ID = fin.VIEW_ID
+        WHERE PERIOD_ID IN (202507,202506,202505)	
+        GROUP BY 	fin.[PERIOD_ID],
+        			fin.[VIEW_ID],
+        			vw.VIEW_LABEL
+        ORDER BY 	fin.[PERIOD_ID],
+        			fin.[VIEW_ID],
+        			vw.VIEW_LABEL;"""
+    """
+    df = run_query(query)
+    st.dataframe(df)
