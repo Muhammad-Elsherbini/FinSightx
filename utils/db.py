dc41c0b4c8e2ea5ebe6bf3cf69dbde94dd624677
Muhammad M. Elsherbini <61868581+Muhammad-Elsherbini@users.noreply.github.com>
2025-08-10 14:32:47 +0300

    Update db.py

--- utils/db.py
+++ utils/db.py
@@ -1,1 +1,22 @@
+import pandas as pd
+import pyodbc
+import streamlit as st
+
+def get_connection():
+    conn_str = (
+        f"DRIVER={{{st.secrets['driver']}}};"
+        f"SERVER={st.secrets['server']};"
+        f"DATABASE={st.secrets['database']};"
+        f"UID={st.secrets['username']};"
+        f"PWD={st.secrets['password']};"
+        "Encrypt=yes;"
+        "TrustServerCertificate=no;"
+    )
+    return pyodbc.connect(conn_str)
+
+def run_query(query):
+    conn = get_connection()
+    df = pd.read_sql(query, conn)
+    conn.close()
+    return df
 
